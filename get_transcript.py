bbc2ce57b178a11a99ba80cb78cb59e73d1ef497
Callum Ferguson <Calxferg@gmail.com>
2025-03-31 14:43:45 -0700

    get transcript with proxy

--- get_transcript.py
+++ get_transcript.py
@@ -1,8 +1,24 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, request, jsonify
-
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.proxies import WebshareProxyConfig
 
 app = Flask(__name__)
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Retrieve the environment variables
+proxy_username = os.getenv("WEBSHARE_PROXY_USERNAME")
+proxy_password = os.getenv("WEBSHARE_PROXY_PASSWORD")
+
+ytt_api = YouTubeTranscriptApi(
+    proxy_config=WebshareProxyConfig(
+        proxy_username=proxy_username,
+        proxy_password=proxy_password,
+    )
+)
 
 
 @app.route("/transcript", methods=["GET"])
@@ -12,7 +28,7 @@ def get_transcript():
         return jsonify({"error": "The video_id query parameter is required."}), 400
 
     try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        transcript = ytt_api.fetch(video_id)
         # transcript = [
         #     {
         #         "duration": 6.479,
